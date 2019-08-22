b9250f60cff21c1a4ddac969bf27c55ed5b2f19f
Alvar <post@0x21.biz>
2019-08-22 13:41:29 +0200

    Alarm class

--- alarm/__init__.py
+++ alarm/__init__.py
@@ -3,23 +3,37 @@ import interrupt
 import utime
 import vibra
 
-def show_display(i):
-    with display.open() as disp:
-        disp.clear()
-        disp.print("{}".format(i))
-        disp.update()
-        disp.close()
+class Alarm:
+    def __init__(self, limit):
+        self._limit = limit
 
-def alarm_fun(x):
-    show_display("0w0")
-    vibra.vibrate(200)
+        interrupt.set_callback(interrupt.RTC_ALARM, self._alarm_handler)
+        interrupt.enable_callback(interrupt.RTC_ALARM)
 
-interrupt.set_callback(interrupt.RTC_ALARM, alarm_fun)
-interrupt.enable_callback(interrupt.RTC_ALARM)
+        self._display_time()
+        utime.alarm(utime.time() + 1)
 
-utime.alarm(utime.time() + 5)
+    def _alarm_handler(self, x):
+        self._limit -= 1
+        if self._limit == 0:
+            self._display("uwu")
+            vibra.vibrate(200)
+        else:
+            self._display_time()
+            utime.alarm(utime.time() + 1)
 
-show_display("gumo, t-5")
+    def _display(self, msg):
+        with display.open() as disp:
+            disp.clear()
+            disp.print("{}".format(msg))
+            disp.update()
+            disp.close()
+
+    def _display_time(self):
+        self._display("T-{}".format(self._limit))
+
+
+Alarm(5)
 
 while True:
     pass
