72766b827ae337899fa41539038cf1b15d055e8a
Alvar <post@0x21.biz>
2019-08-24 15:36:13 +0200

    Matrix: multiple lines

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -5,17 +5,18 @@ import urandom
 
 
 COLORS = [(0, 153, 51), (0, 153, 0), (51, 204, 51), (0, 102, 0), (102, 255, 102)]
-STEP_SIZE = 3
+LINES = 40
 
 
 class Line:
     def __init__(self):
         self._x = urandom.randint(0, 160)
-        self._y = urandom.randint(0, 80)
+        self._y = urandom.randint(0, 20)
+        self._speed = urandom.randint(1, 15)
         self._elems = []
 
     def update_check(self):
-        self._y += STEP_SIZE
+        self._y += self._speed
         self._elems.append(urandom.choice(COLORS))
 
         return self._y < 80
@@ -35,16 +36,28 @@ class Line:
             return ((self._x, self._y + self._iter_id), col)
 
 
-l = Line()
+class LineManager:
+    def __init__(self):
+        self._lines = []
+        for l in range(LINES):
+            self._lines.append(Line())
+
+    def draw(self):
+        with display.open() as disp:
+            disp.clear()
+
+            for i in range(LINES):
+                if not self._lines[i].update_check():
+                    self._lines[i] = Line()
+
+                for p in self._lines[i]:
+                    disp.pixel(p[0][0], p[0][1],
+                            col=color.Color(p[1][0], p[1][1], p[1][2]))
+
+            disp.update()
+
+
+lm = LineManager()
 while True:
-    if not l.update_check():
-        l = Line()
-
-    with display.open() as disp:
-        disp.clear()
-        for p in l:
-            disp.pixel(p[0][0], p[0][1],
-                    col=color.Color(p[1][0], p[1][1], p[1][2]))
-        disp.update()
-
-    utime.sleep(0.1)
+    lm.draw()
+    utime.sleep(0.05)
