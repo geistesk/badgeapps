49d6fccc31507ec60ab302868d10662e6b8e7486
Alvar <post@0x21.biz>
2019-08-24 13:25:46 +0200

    Alarm: vibrate and exit

--- alarm/__init__.py
+++ alarm/__init__.py
@@ -5,69 +5,71 @@ import interrupt
 import utime
 import vibra
 
+def show_time(t):
+    col_text_bg = color.Color(0, 0, 255)
+    col_text_fg = color.Color(255, 255, 255)
+    col_time_bg = color.Color(0, 128, 255)
+    col_time_fg = color.Color(255, 255, 255)
+
+    t_min, t_sec = int(t / 60), int(t % 60)
+    t_msg = "{:02d} : {:02d}".format(t_min, t_sec)
+
+    with display.open() as disp:
+        disp.clear(col_text_bg)
+        disp.print("alarm", fg=col_text_fg, bg=col_text_bg, posx=47)
+        disp.rect(0, 20, 160, 60, col=col_time_bg)
+        disp.print(t_msg, fg=col_time_fg, bg=col_time_bg, posx=35, posy=30)
+        disp.update()
+
+
 class Alarm:
     def __init__(self, limit):
         self._limit = limit
 
-        interrupt.set_callback(interrupt.RTC_ALARM, self._alarm_handler)
+        interrupt.set_callback(interrupt.RTC_ALARM, self._countdown_handler)
         interrupt.enable_callback(interrupt.RTC_ALARM)
 
-        self._display_time()
         utime.alarm(utime.time() + 1)
 
-    def _alarm_handler(self, x):
+    def _countdown_handler(self, x):
         self._limit -= 1
-        if self._limit == 0:
-            self._display("uwu")
-            vibra.vibrate(200)
-        else:
-            self._display_time()
+        show_time(self._limit)
+
+        if self._limit > 0:
             utime.alarm(utime.time() + 1)
+        else:
+            self._alarm_handler()
 
-    def _display(self, msg):
-        with display.open() as disp:
-            disp.clear()
-            disp.print("{}".format(msg))
-            disp.update()
+    def _alarm_handler(self):
+        while True:
+            vibra.vibrate(100)
+            utime.sleep_ms(100)
 
-    def _display_time(self):
-        self._display("T-{}".format(self._limit))
+            pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT | \
+                    buttons.TOP_RIGHT | buttons.TOP_LEFT)
+            if pressed != 0:
+                break
 
 
 class SetupMenu:
     def __init__(self):
         self._time_val = 0
 
-    def get_alarm(self):
+    def start_alarm(self):
         while True:
-            self._show_setup_time()
+            show_time(self._time_val)
 
             pressed = buttons.read(
                     buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT | buttons.TOP_RIGHT)
             if pressed & buttons.BOTTOM_LEFT != 0:
-                self._time_val = max(0, self._time_val - 15)
+                self._time_val = max(0, self._time_val - 5)
             elif pressed & buttons.BOTTOM_RIGHT != 0:
-                self._time_val += 15
+                self._time_val += 5
             elif pressed & buttons.TOP_RIGHT != 0:
                 return Alarm(self._time_val)
 
-    def _show_setup_time(self):
-        col_text_bg = color.Color(0, 0, 255)
-        col_text_fg = color.Color(255, 255, 255)
-        col_time_bg = color.Color(0, 128, 255)
-        col_time_fg = color.Color(255, 255, 255)
 
-        t_min, t_sec = int(self._time_val / 60), int(self._time_val % 60)
-        t_msg = "{:02d} : {:02d}".format(t_min, t_sec)
+SetupMenu().start_alarm()
 
-        with display.open() as disp:
-            disp.clear(col_text_bg)
-            disp.print("set alarm", fg=col_text_fg, bg=col_text_bg, posx=17)
-            disp.rect(0, 20, 160, 60, col=col_time_bg)
-            disp.print(t_msg, fg=col_time_fg, bg=col_time_bg, posx=35, posy=30)
-            disp.update()
-
-
-alarm = SetupMenu().get_alarm()
 while True:
     pass
