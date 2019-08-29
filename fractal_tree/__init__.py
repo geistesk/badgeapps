82fae0604e6f66a971f69e1a8f486520722c6f9c
Alvar <post@0x21.biz>
2019-08-29 14:13:04 +0200

    Inital port of fractal_tree to the card10

--- fractal_tree/__init__.py
+++ fractal_tree/__init__.py
@@ -1,37 +1,84 @@
-from math   import sin, cos, radians
-from random import randint, seed, uniform
-from time   import sleep
-import ugfx
+# Const to determine which badge is used
+SHA17 = 0
+CCC19 = 1
+BADGE = None
 
-EDGE_RIGHT_DOWN = (296, 128)
+try:
+    import ugfx
+    BADGE = SHA17
+except:
+    import display
+    BADGE = CCC19
 
-def tree(point, level, angle):
+if BADGE == SHA17:
+    import ugfx
+    from math   import sin, cos, radians
+    from random import randint, seed, uniform
+    from time   import sleep
+
+    EDGE_RIGHT_DOWN = (296, 128)
+elif BADGE == CCC19:
+    import display
+    from math    import sin, cos, radians
+    from urandom import randint, seed, uniform
+    from utime   import sleep
+
+    EDGE_RIGHT_DOWN = (159, 79)
+else:
+    raise ValueError("Unknown badge")
+
+
+def sanitize_point(point):
+    f = lambda x, no: max(min(EDGE_RIGHT_DOWN[no], x), 0)
+    return (f(point[0], 0), f(point[1], 1))
+
+
+def tree(point, level, angle, disp):
     if level > 0:
         point_new = (
-          point[0] + int(cos(radians(angle)) * level * 3.2),
-          point[1] + int(sin(radians(angle)) * level * 2.0))
+            point[0] + int(cos(radians(angle)) * level * 3.2),
+            point[1] + int(sin(radians(angle)) * level * 2.0))
         rand_left, rand_right = (uniform(0.5, 1.5), uniform(0.5, 1.5))
 
-        ugfx.line(point[0], point[1], point_new[0], point_new[1], ugfx.WHITE)
-        tree(point_new, level - 1, angle - 20 * rand_left)
-        tree(point_new, level - 1, angle + 20 * rand_right)
+        if BADGE == SHA17:
+            ugfx.line(point[0], point[1], point_new[0], point_new[1], ugfx.WHITE)
+        elif BADGE == CCC19:
+            p0 = sanitize_point(point)
+            p1 = sanitize_point(point_new)
 
-ugfx.init()
-ugfx.clear(ugfx.WHITE)
-ugfx.flush()
-ugfx.clear(ugfx.BLACK)
-ugfx.flush()
+            disp.line(p0[0], p0[1], p1[0], p1[1], col=(128, 128, 128))
 
-ugfx.input_init()
-ugfx.input_attach(ugfx.BTN_A, lambda p: quit(p))
-ugfx.input_attach(ugfx.BTN_B, lambda p: quit(p))
-ugfx.input_attach(ugfx.BTN_START, lambda p: quit(p))
-ugfx.input_attach(ugfx.BTN_SELECT, lambda p: quit(p))
+        tree(point_new, level - 1, angle - 20 * rand_left, disp)
+        tree(point_new, level - 1, angle + 20 * rand_right, disp)
 
-while True:
-    for i in range(0, randint(4, 8)):
-        tree(EDGE_RIGHT_DOWN, randint(4, 14), -155)
-        sleep(randint(3, 13) * 0.1)
-        ugfx.flush()
+
+if BADGE == SHA17:
+    ugfx.init()
+    ugfx.clear(ugfx.WHITE)
+    ugfx.flush()
     ugfx.clear(ugfx.BLACK)
     ugfx.flush()
+
+    ugfx.input_init()
+    ugfx.input_attach(ugfx.BTN_A, lambda p: quit(p))
+    ugfx.input_attach(ugfx.BTN_B, lambda p: quit(p))
+    ugfx.input_attach(ugfx.BTN_START, lambda p: quit(p))
+    ugfx.input_attach(ugfx.BTN_SELECT, lambda p: quit(p))
+
+while True:
+    if BADGE == CCC19:
+        disp = display.open()
+    else:
+        disp = None
+
+    for i in range(0, randint(4, 8)):
+        tree(EDGE_RIGHT_DOWN, randint(4, 14), -155, disp)
+        sleep(randint(3, 13) * 0.1)
+        # ugfx.flush()  # TODO
+
+    if BADGE == SHA17:
+        ugfx.clear(ugfx.BLACK)
+        ugfx.flush()
+    elif BADGE == CCC19:
+        disp.update()
+        disp.close()
